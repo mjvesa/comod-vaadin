2eb16834c3cc741e30491a726fa02cce961eac5e
Matti Vesa <mattijvesa@gmail.com>
2022-03-17 22:30:21 +0200

    Add initial version of import mappings

---
+++ src/component_imports.js
@@ -0,0 +1,111 @@
+export const vaadinComponentImports = {
+  "vaadin-accordion": [
+    'import "@vaadin/vaadin-accordion/theme/lumo/vaadin-accordion.js"',
+  ],
+  "vaadin-notification": [
+    'import "@vaadin/vaadin-notification/theme/lumo/vaadin-notification.js"',
+  ],
+  "vaadin-checkbox": [
+    'import "@vaadin/vaadin-checkbox/theme/lumo/vaadin-checkbox.js"',
+  ],
+  "vaadin-cehckbox-group": [
+    'import "@vaadin/vaadin-checkbox/theme/lumo/vaadin-checkbox-group.js"',
+  ],
+  "vaadin-button": [
+    'import "@vaadin/vaadin-button/theme/lumo/vaadin-button.js"',
+  ],
+  "vaadin-overlay": [
+    'import "@vaadin/vaadin-overlay/theme/lumo/vaadin-overlay.js"',
+  ],
+  "vaadin-date-picker": [
+    'import "@vaadin/vaadin-date-picker/theme/lumo/vaadin-date-picker.js"',
+  ],
+  "vaadin-split-layout": [
+    'import "@vaadin/vaadin-split-layout/theme/lumo/vaadin-split-layout.js"',
+  ],
+  "vaadin-progress-bar": [
+    'import "@vaadin/vaadin-progress-bar/theme/lumo/vaadin-progress-bar.js"',
+  ],
+  "vaadin-combo-box-item": [
+    'import "@vaadin/vaadin-combo-box/src/vaadin-combo-box-item.js"',
+  ],
+  "vaadin-combo-box-light": [
+    'import "@vaadin/vaadin-combo-box/theme/lumo/vaadin-combo-box-light.js"',
+  ],
+  "vaadin-combo-box-dropdown": [
+    'import "@vaadin/vaadin-combo-box/src/vaadin-combo-box-dropdown.js"',
+  ],
+  "vaadin-combo-box-dropdown-wrapper": [
+    'import "@vaadin/vaadin-combo-box/src/vaadin-combo-box-dropdown-wrapper.js"',
+  ],
+  "vaadin-combo-box": [
+    'import "@vaadin/vaadin-combo-box/theme/lumo/vaadin-combo-box.js"',
+  ],
+  "vaadin-custom-field": [
+    'import "@vaadin/vaadin-custom-field/theme/lumo/vaadin-custom-field.js"',
+  ],
+  "vaadin-text-field": [
+    'import "@vaadin/vaadin-text-field/theme/lumo/vaadin-text-field.js"',
+  ],
+  "vaadin-number-field": [
+    'import "@vaadin/vaadin-text-field/theme/lumo/vaadin-number-field.js"',
+  ],
+  "vaadin-email-field": [
+    'import "@vaadin/vaadin-text-field/theme/lumo/vaadin-email-field.js"',
+  ],
+  "vaadin-time-picker": [
+    'import "@vaadin/vaadin-time-picker/theme/lumo/vaadin-time-picker.js"',
+  ],
+  "vaadin-password-field": [
+    'import "@vaadin/vaadin-text-field/theme/lumo/vaadin-password-field.js"',
+  ],
+  "vaadin-text-area": [
+    'import "@vaadin/vaadin-text-field/theme/lumo/vaadin-text-area.js"',
+  ],
+  "vaadin-context-menu": [
+    'import "@vaadin/vaadin-context-menu/theme/lumo/vaadin-context-menu.js"',
+  ],
+  "vaadin-device-detector": [
+    'import "@vaadin/vaadin-context-menu/src/vaadin-device-detector.js"',
+  ],
+  "vaadin-context-menu-overlay": [
+    'import "@vaadin/vaadin-context-menu/src/vaadin-context-menu-overlay.js"',
+  ],
+  "vaadin-tabs": ['import "@vaadin/vaadin-tabs/theme/lumo/vaadin-tabs.js"'],
+  "vaadin-tab": ['import "@vaadin/vaadin-tabs/theme/lumo/vaadin-tab.js"'],
+  "vaadin-horizontal-layout": [
+    'import "@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-horizontal-layout.js"',
+  ],
+  "vaadin-vertical-layout": [
+    'import "@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-vertical-layout.js"',
+  ],
+  "vaadin-form-layout": [
+    'import "@vaadin/vaadin-form-layout/theme/lumo/vaadin-form-layout.js"',
+  ],
+  "vaadin-form-item": [
+    'import "@vaadin/vaadin-form-layout/theme/lumo/vaadin-form-item.js"',
+  ],
+  "vaadin-list-box": [
+    'import "@vaadin/vaadin-list-box/theme/lumo/vaadin-list-box.js"',
+  ],
+  "vaadin-select": ['import "@vaadin/vaadin-select/src/vaadin-select.js"'],
+  "vaadin-upload-file": [
+    'import "@vaadin/vaadin-upload/src/vaadin-upload-file.js"',
+  ],
+  "vaadin-upload": [
+    'import "@vaadin/vaadin-upload/theme/lumo/vaadin-upload.js"',
+  ],
+  "vaadin-dialog": [
+    'import "@vaadin/vaadin-dialog/theme/lumo/vaadin-dialog.js"',
+  ],
+  "vaadin-radio-group": [
+    'import "@vaadin/vaadin-radio-button/theme/lumo/vaadin-radio-group.js"',
+  ],
+  "vaadin-radio-button": [
+    'import "@vaadin/vaadin-radio-button/theme/lumo/vaadin-radio-button.js"',
+  ],
+  "vaadin-icon": [
+    'import "@vaadin/vaadin-icons"',
+    'import "@vaadin/vaadin-icon"',
+  ],
+};
